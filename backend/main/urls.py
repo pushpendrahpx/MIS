8fa064f4b81a2c9d822ab896479bbde97a5bd0d6
Pushpendrahpx <pushpendra.hpx2001@gmail.com>
2020-01-13 11:52:35 +0530

    mychanges

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
-
 router = routers.DefaultRouter()
 router.register('userinfo', views.UserInfoView)
 router.register('books', views.BooksView)
@@ -12,6 +11,7 @@ router.register('books', views.BooksView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users',views.get_user,name='json'),
     path('api/user/', views.json_create_user, name='json'),
     path('api/books/', views.json_create_books, name='json'),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from django.core import serializers
 import json
 
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 class UserInfoView(viewsets.ModelViewSet):
     queryset = UserInfo.objects.all()
@@ -18,16 +19,33 @@ class BooksView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_class = (permissions.IsAuthenticatedOrReadOnly)
 
-
-def json_create_user(request):
-    ls = UserInfo.objects.all()
-    if request.method == 'POST':    
+@ensure_csrf_cookie
+def get_user(request):
+    if request.method == 'POST':
+        print(request.POST['phone'])
+        ls = UserInfo.objects.all()
         ls_seiral = serializers.serialize('json', ls)
         json_data = json.dumps(ls_seiral)
         json_data = json.loads(json_data)
         json_data = json.loads(json_data)
+        return JsonResponse(json_data, safe=False)
+    else:
+        return JsonResponse('{"name":"Failed POST bro, it is GET"}',safe=False)
+    
+
+def json_create_user(request):
+    ls = UserInfo.objects.all()
+    if request.method == 'POST':   
+        # name = str(request.POST['name'])
+        phone = int(request.POST['phone']) 
+        ls_seiral = serializers.serialize('json', ls)
+        json_data = json.dumps(ls_seiral)
+        json_data = json.loads(json_data)
+        json_data = json.loads(json_data)
+        json_data = '{"name":"GVHJBN"}'
         u = UserInfo(**json_data)
         u.save()
+        
     else:
         json_data = ''
     return JsonResponse(json_data, safe=False)
